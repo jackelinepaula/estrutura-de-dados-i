a5deb9c3b4c2b7104d895df4746d6c2459b9a732
Jackeline Paula <56116537+jackelinepaula@users.noreply.github.com>
2023-02-16 10:47:44 -0300

    loop

---
+++ whileloop.java
@@ -0,0 +1,38 @@
+//import java.util.Scanner;
+//public class whileloop {
+//    public static void main(String args[]){
+//        Scanner scan = new Scanner(System.in);
+////        //Jeito pensante
+////        int i = 1;
+////        while(i <= 10){
+////
+////            if(i % 2 == 0){
+////                System.out.println(i);
+////            }
+////            i++;
+////        }
+////
+////        //O mais óbvio
+////        int j = 2;
+////        while(j <= 10){
+////            System.out.println(j);
+////            j += 2;
+////        }
+//
+//        //---------------------
+//
+//        System.out.println("\nEscreva um número");
+//        int n = scan.nextInt();
+//
+//        int i = n - 1;
+//
+//        while(i > 1){
+//            n = n * i;
+//            System.out.println(n);
+//            i--;
+//        }
+//
+//
+//        System.out.println("\nFine");
+//    }
+//}
