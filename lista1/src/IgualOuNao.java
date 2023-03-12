6a2e429e934179e31836bbed7177fbfd5bec1159
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2023-03-12 12:51:14 -0300

    lista 1

---
+++ lista1/src/IgualOuNao.java
@@ -0,0 +1,24 @@
+package lista1.src;
+
+import java.util.Scanner;
+public class IgualOuNao {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Entre com o valor A");
+        a = scan.nextInt();
+        System.out.println("Agora o valor B");
+        b = scan.nextInt();
+
+        if(a > b){
+            System.out.println("A é maior que B");
+        }else if(b > a){
+            System.out.println("B é maior que A");
+        }else{
+            System.out.println("Os valores são iguais");
+        }
+
+    }
+}
---
+++ lista1/src/MeioLosango.java
@@ -0,0 +1,18 @@
+package lista1.src;
+
+public class MeioLosango {
+    public static void main(String[] args){
+        int i = 0;
+        String string = "*";
+
+        for(i = 0; i < 5; i++){
+            System.out.println(string);
+            string = string+"*";
+        }
+
+        for(i = 5; i >= 0; i--){
+            string = string.substring(0, string.length() - 1);
+            System.out.println(string);
+        }
+    }
+}
