5a70650e1c135e482c76d0e24a29e60349496bae
jackelinepaula <jackeline.menezes@fatec.sp.gov.br>
2023-03-16 09:34:05 -0300

    stack

--- stack/src/Main.java
+++ stack/src/Main.java
@@ -1,16 +1,35 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println(mult(4, 2));
+//        System.out.println(mult(4, 2));
+
     }
 
-    public static double mult(double v1, double v2){
+//    public static double mult(double v1, double v2){
+//
+//        double resul = 0;
+//
+//        for(int i = 1; i <= v1; i++){
+//            resul = resul + v2;
+//        }
+//
+//        return resul;
+//    }
 
-        double resul = 0;
+    //Recurisvidade
+    static double multRec(double v1, double v2){
 
-        for(int i = 1; i <= v1; i++){
-            resul = resul + v2;
+        if(v2 == 1){
+            return v1;
         }
+        else{
+            return v1 + multRec(v1, v2-1);
 
-        return resul;
+//            Ex com 4 e 2
+//                4 + multiRec(v1, v2-1)
+//                    4 + 4 -> (multRec(v1, v2-1))
+
+
+        }
     }
+
 }
